93f7a0b103894b9368fa79367b9d812fa807319a
allenhichard <allenhichard21@gmail.com>
2020-07-09 23:31:55 -0300

    Configurando rotas da aplicação e criando os componentes home e registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//import {AppComponent} from "./app.component"
+import{LoginComponent} from "./login/login.component"
+import {ContasComponent} from "./contas/contas.component"
+import {RegistroComponent} from "./registro/registro.component"
+import {HomeComponent} from "./home/home.component"
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: LoginComponent},
+  {path: "contas", component: ContasComponent},
+  {path: "registro", component: RegistroComponent},
+  {path: "home", component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContasComponent } from './contas/contas.component';
 import { ContasService } from "./contas.service";
-import { LoginComponent } from './login/login.component'
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     ContasComponent,
-    LoginComponent
+    LoginComponent,
+    RegistroComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
